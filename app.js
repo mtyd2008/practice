8540d9791e0669d35afd4814d025df3d2f58e610
mtyd2008 <tahaydandya@gmail.com>
2025-03-13 20:06:35 +0500

    Api practise

---
+++ app.js
@@ -0,0 +1,21 @@
+// console.log("hello world!");
+
+// <{--PROBLEMS--}>
+
+// 1)Create a debounce function that limits the rate at which a function is executed.
+
+// function debounce() {
+//     setTimeout(() => {
+//        console.log("hello");
+        
+//     }, 1000);
+// }
+
+// console.log(debounce());
+
+// 2) Create a function myPromiseAll that mimics the behavior of Promise.all, taking an array of promises
+//  and resolving them together.
+
+// const myPromiseAll = (promises) => {
+    
+// }
